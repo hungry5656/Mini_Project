1d01ed852cd9cd70436c6e7452f08cc09765911c
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-11 21:25:49 -0700

    add some functions

--- 122A_EvenListGame/linkedList.cpp
+++ 122A_EvenListGame/linkedList.cpp
@@ -1,1 +1,60 @@
 #include "linkedList.h"
+
+linkedListNode::linkedListNode(int val):
+    value(val),
+    parent_(std::weak_ptr<linkedListNode>()),
+    child_(nullptr){}
+
+linkedListNode::linkedListNode(int val, std::weak_ptr<linkedListNode> parent):
+    value(val),
+    parent_(parent),
+    child_(nullptr){}
+
+bool linkedListNode::isEnd(){
+    return child_ == nullptr;
+}
+
+linkedList::linkedList():
+    root(nullptr),
+    size_(0){}
+
+linkedList::linkedList(std::vector<int> vec){
+    size_ = 0;
+    for(auto itr = vec.begin(); itr != vec.end(); ++itr){
+        insert(*itr);
+        ++size_;
+    }
+}
+
+void linkedList::insert(int val){
+    if (root == nullptr) {
+		root = std::make_shared<linkedListNode>(val);
+		size_++;
+		return;
+	}
+}
+
+int linkedList::findfirst() const{
+    return root -> value;
+}
+
+void linkedList::deleteFront(){
+    
+}
+
+void linkedList::deleteEnd(){
+
+}
+
+int linkedList::findLast() const{
+    if (root == nullptr) return -1;
+    std::shared_ptr<linkedListNode> currentNode = root;
+    while (!currentNode->isEnd()){
+        currentNode = currentNode -> child_;
+    }
+    return currentNode -> value;
+}
+
+size_t linkedList::size() const{
+    return size_;
+}
--- 122A_EvenListGame/linkedList.h
+++ 122A_EvenListGame/linkedList.h
@@ -6,20 +6,26 @@ class linkedListNode{
     public:
         linkedListNode(int val);
         linkedListNode(int val, std::weak_ptr<linkedListNode> parent);
-
+        void deletChild();
         bool isEnd();
 
     private:
         std::weak_ptr<linkedListNode> parent_;
         std::shared_ptr<linkedListNode> child_;
+        int value;
+    
     friend linkedList;
 };
 
 class linkedList{
     public:
+        linkedList();
+        linkedList(int val);
         linkedList(std::vector<int> vec);
 
         void insert(int val);
+        void deleteFront();
+        void deleteEnd();
         int findfirst() const;
         int findLast() const;
         size_t size() const;
