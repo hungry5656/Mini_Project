dfd27825403deaf876c036697712d60626ed7d6a
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-26 13:28:23 -0700

    update

--- 122A_MergeAndCountInv/MergeAndCount.cpp
+++ 122A_MergeAndCountInv/MergeAndCount.cpp
@@ -1,5 +1,6 @@
 #include "MergeAndCount.h"
 
+
 std::vector<int> MergeAndCount(std::vector<int> A, std::vector<int> B, int* NumInv){
     /*
     ** This function Merge the vector and count the number of inversion
@@ -18,5 +19,6 @@ std::vector<int> SortAndCount(std::vector<int> List, int* NumInv){
     std::vector<int> B(List.begin() + HalfIndex + 1, List.end());
     A = SortAndCount(A, NumInv);
     B = SortAndCount(B, NumInv);
-    List = MergeAndCount(A, B)
+    List = MergeAndCount(A, B, NumInv);
+    
 }
