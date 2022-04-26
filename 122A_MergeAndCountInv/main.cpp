8c59045dcf1c71311c62215a2355cf6a559ecc88
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-26 00:25:39 -0700

    update code

--- 122A_MergeAndCountInv/main.cpp
+++ 122A_MergeAndCountInv/main.cpp
@@ -1,9 +1,12 @@
-#include <string>
-#include <iostream>
+#include <string> // string
+#include <iostream> // cout
+#include <cstdlib> // rand()
 #define NUM_RANGE_ 99
 #define VEC_SIZE_ 99
 
 int main(){
-    int vecSize = 
+    int vecSize = std::rand() % VEC_SIZE_;
+    std::vector<int> 
+    for (int i = 0; i < vecSize)
     return 0;
 }
