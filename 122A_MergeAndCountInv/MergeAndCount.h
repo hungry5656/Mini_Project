139f5417901bfd045c933349898c49155686d0b9
hungry5656 <hhhhhenrysun@gmail.com>
2022-04-26 01:08:18 -0700

    update code

---
+++ 122A_MergeAndCountInv/MergeAndCount.h
@@ -0,0 +1,6 @@
+#include <string> // string
+#include <iostream> // cout
+#include <cstdlib> // rand()
+#include <vector> // vector
+
+std::vector<int> MergeAndCount(int test);
--- 122A_MergeAndCountInv/main.cpp
+++ 122A_MergeAndCountInv/main.cpp
@@ -1,12 +1,18 @@
+#include "MergeAndCount.h"
 #include <string> // string
 #include <iostream> // cout
 #include <cstdlib> // rand()
+#include <vector> // vector
 #define NUM_RANGE_ 99
 #define VEC_SIZE_ 99
 
 int main(){
     int vecSize = std::rand() % VEC_SIZE_;
-    std::vector<int> 
-    for (int i = 0; i < vecSize)
+    std::vector<int> TestVector;
+    for (int i = 0; i < vecSize; ++i){
+        TestVector.push_back(std::rand() % NUM_RANGE_);
+        std::cout << TestVector[i] << std::endl;
+    }
+    
     return 0;
 }
